a26be00c2e81218e5d688060e87ccb1952e02b73
KevinDMack <kevinmackdev@outlook.com>
2025-04-16 12:57:10 -0400

    MIgrating functionality into single control panel

--- control-plane/main.py
+++ control-plane/main.py
@@ -1,26 +1,15 @@
 from gpiozero import Button
 from signal import pause
+from utils.flashlight import Flashlight
 
 import subprocess
 
 class ActionPanel:
-    def __init__(self, flashlight_button_pin):
-        self.flashlight_button = Button(flashlight_button_pin)
-        self.flashlight_button.when_pressed = self.flash_light_on_button_pressed
+    def __init__(self, flashlight_button_pin, led_pin):
         self.enabled = False 
         self.flashlight_yaml = "./yamls/flashlight.yaml"
-
-    def flash_light_on_button_pressed(self):
-        print("Flashlight Button pressed")
-        if self.enabled:
-            print("Flashlight Disabled")
-            self.enabled = False
-            self.delete()
-        else:
-            print("Button action disabled")
-            print("Flashlight Enabled")
-            self.enabled = True
-            self.apply()
+        self.flashlight = Flashlight(led_pin=led_pin, button_pin=flashlight_button_pin)
+        self.flashlight.run()
 
     def apply(self):
         print("Applying changes to the flashlight")
@@ -44,9 +33,10 @@ class ActionPanel:
 
     def run(self):
         print("Action Panel is running")
-        pause()
 
 if __name__ == "__main__":
+    flashlight_button_pin = 26
+    led_pin = 4
     print("Setting Action Panel GPIO pins")
-    action_panel = ActionPanel(flashlight_button_pin=18)
+    action_panel = ActionPanel(flashlight_button_pin, led_pin)
     action_panel.run()
